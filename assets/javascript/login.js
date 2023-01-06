d9e553084335c0ff136f5d49dd7775e9b8bc8fdb
ThanhThuyPNV <116275473+ThanhThuyPNV@users.noreply.github.com>
2023-01-06 13:51:00 +0700

    login continue

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -179,7 +179,7 @@ function change_account_none() {
 }
 // login with admin
 
-const url_ADMIN="https://61c16f3d9dbcca0017c81ed3.mockapi.io/api/AcountADMIN";
+const url_ADMIN="https://63a572152a73744b008e2940.mockapi.io/api/admin";
 
 function sign_in_account_admin() {
   var email = document.getElementById("login_email_admin").value;
@@ -211,4 +211,80 @@ function sendEmail(email) {
 	Subject : "Promo code form INDOOR",
 	Body : "Promo code: ABCD",
 	});
+}
+function change_login_none() {
+  // none sau khi đăng nhập
+  document.getElementById("nav__login").style.display = "none";
+  // document.getElementById("nav__login_mobile").style.display = "none";
+
+ }
+function change_account_block() {
+  //block sau khi đăng nhập
+  document.getElementById("nav__account").style.display = "block";
+  // document.getElementById("nav__account_moblile").style.display = "block";
+
+}
+function change_account_none() {
+  // none sau khi đăng xuất
+  document.getElementById("nav__account").style.display = "none";
+  document.getElementById("nav__account_moblile").style.display = "none";
+
+}
+
+// Setting account (xem thông tin người dùng)
+function open_setting_account() {
+  document.getElementById("setting_account").style.display = "block";
+  axios.get(url_customers + "/" + localStorage.getItem(CUSID)).then((res) => {
+    var cus = res.data;
+    document.getElementById("setting_email").innerText = cus.email;
+    document.getElementById("setting_password").innerText = cus.password;
+    document.getElementById("setting_phone").innerText = cus.phone_number;
+    document.getElementById("setting_address").innerText = cus.address;
+  });
+}
+
+// show thông tin người dùng để sửa
+function show_user_info_old() {
+  document.getElementById("setting_account").style.display = "block";
+  axios.get(url_customers + "/" + localStorage.getItem(CUSID)).then((res) => {
+    var cus = res.data;
+    document.getElementById("edit_email").value = cus.axiosemail;
+    document.getElementById("edit_password").value = cus.password;
+    document.getElementById("edit_phone").value = cus.phone_number;
+    document.getElementById("edit_address").value = cus.address;
+  });
+}
+// cập nhập thông tin sau khi sửa
+function done_edit_info() {
+  var email = document.getElementById("edit_email").value;
+  var password = document.getElementById("edit_password").value;
+  var phone = document.getElementById("edit_phone").value;
+  var address = document.getElementById("edit_address").value;
+  var formData = {
+    email: email,
+    password: password,
+    phone_number: phone,
+    address: address,
+  };
+  axios
+    .put(url_customers + "/" + localStorage.getItem(CUSID), formData)
+    .then(() => {
+      location.reload();
+    });
+}
+
+function close_setting_account() {
+  document.getElementById("setting_account").style.display = "none";
+}
+
+
+// chuyển đổi giữa edit và done;
+function open_form_edit() {
+  document.getElementById("edit__account").style.display = "block";
+  document.getElementById("your__account").style.display = "none";
+}
+
+function close_form_edit() {
+  document.getElementById("edit__account").style.display = "none";
+  document.getElementById("your__account").style.display = "block";
 }
